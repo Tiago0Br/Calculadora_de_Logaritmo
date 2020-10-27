6236b47efb24e7927ed36c6e3704680228d7d913
Tiago Lopes <52180428+Tiago0Br@users.noreply.github.com>
2020-10-27 18:07:45 -0300

    Adicionando funções para calcular cada caso.

--- calculator.cpp
+++ calculator.cpp
@@ -5,6 +5,9 @@
 using namespace std;
 
 // Declaração das funções
+void calcular_logaritmo(float a, float b);
+void calcular_logaritmando(float a, float x);
+void calcular_base(float b, float x);
 void escrever(string texto);
 
 int main() {
@@ -34,28 +37,7 @@ int main() {
             scanf("%f", &b);
             printf("Log %.2f = x \n", b);
             printf("    %.2f \n\n", a);
-
-            if(a==1) {
-                printf("A base não pode ser igual a 1! \n");
-            }
-
-            else if(a==b){
-                printf("Quando o logaritmando e a base forem iguais o logaritmo será 1 \n");
-                x = 1;
-            }
-
-            else if (b==1) {
-                printf("Quando logaritmando for 1 o logaritmo sempre será 0 \n");
-            }
-
-            else {
-            // calcular o valor x para dar o resultado
-                x = log(b)/log(a);
-                printf("Logaritmo = %.2f \n", x);
-            }
-
-            printf("\nLog %.2f = %.2f \n", b, x);
-            printf("    %.2f", a);
+            calcular_logaritmo(a, b);
 
             break;
 
@@ -69,25 +51,12 @@ int main() {
             scanf("%f", &x);
             printf("Log b = %.2f \n", x);
             printf("    %.2f \n\n", a);
+            calcular_logaritmando(a, x);
 
-            if (x==0){
-                printf ("Todo logaritmo = 0 o logaritmando será 1.\n");
-            } else if(a==1) {
-                printf("A base não pode ser igual a 1! \n");
-            } else {
-                b = 1;
-                for(int i=0; i<(x); i++) {
-                    b *= a;
-                }
-                printf("O logaritmando de base %.2f é %.2f", a, b);
-            }
-
-            printf("\nLog %.2f = %.2f \n", b, x);
-            printf("    %.2f", a);
             break;
 
         case 3:
-            // Logaritmando e logarito
+            // Logaritmando e logaritmo
             printf("Digite o valor do logaritmando: ");
             scanf("%f", &b);
             printf("Log %.2f = x \n", b);
@@ -96,18 +65,8 @@ int main() {
             scanf("%f", &x);
             printf("Log %.2f = %.2f \n", b, x);
             printf("    a \n\n");
+            calcular_base(b, x);
 
-            if(x==1){
-                printf("Se o Logaritmo = 1, Então a Base = %.2f. \n", b);
-            }
-            else{
-                a = pow(b, 1.0/x);
-                printf("Log: %.2f = %.2f \n", b, x);
-                printf("Base: %.2f \n", a);
-            }
-
-            printf("\nLog %.2f = %.2f \n", b, x);
-            printf("    %.2f", a);
             break;
         default:
             // Deve haver um tratamento de erros caso o usuário digite um valor inválido
@@ -120,4 +79,66 @@ int main() {
 // Escreve um texto passado como parâmetro quebrando linha no final
 void escrever(string texto) {
     cout << texto << "\n";
+}
+
+// Dado a base e o logaritmando, calcula o logaritmo
+void calcular_logaritmo(float a, float b) {
+    float x;
+    if(a==1) {
+        printf("A base não pode ser igual a 1! \n");
+    }
+    else if(a==b){
+        printf("Quando o logaritmando e a base forem iguais o logaritmo será 1 \n");
+        x = 1;
+    }
+
+    else if (b==1) {
+        printf("Quando logaritmando for 1 o logaritmo sempre será 0 \n");
+    }
+
+    else {
+        // calcular o valor x para dar o resultado
+        x = log(b)/log(a);
+        printf("Logaritmo = %.2f \n", x);
+    }
+
+    printf("\nLog %.2f = %.2f \n", b, x);
+    printf("    %.2f", a);
+
+}
+
+// Dado a base e o logaritmo, calcula o logaritmando
+void calcular_logaritmando(float a, float x) {
+    float b;
+    if (x==0){
+        printf ("Todo logaritmo = 0 o logaritmando será 1.\n");
+    } else if(a==1) {
+        printf("A base não pode ser igual a 1! \n");
+    } else {
+        b = 1;
+        for(int i=0; i<(x); i++) {
+            b *= a;
+        }
+        printf("O logaritmando de base %.2f é %.2f", a, b);
+    }
+
+    printf("\nLog %.2f = %.2f \n", b, x);
+    printf("    %.2f", a);
+}
+
+// Dado o logaritmando e o logaritmo, calcula a base
+void calcular_base(float b, float x) {
+    float a;
+    if(x==1){
+        printf("Se o Logaritmo = 1, Então a Base = %.2f. \n", b);
+        a = b;
+    }
+    else {
+        a = pow(b, 1.0/x);
+        printf("Log: %.2f = %.2f \n", b, x);
+        printf("Base: %.2f \n", a);
+    }
+
+    printf("\nLog %.2f = %.2f \n", b, x);
+    printf("    %.2f", a);
 }
